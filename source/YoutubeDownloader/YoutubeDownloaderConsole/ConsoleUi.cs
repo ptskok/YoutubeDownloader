e2c0ba50fda4e5379920420a41b603c0db20e9eb
Peter Skok - VSC <ptskokmine@gmail.com>
2022-06-15 01:22:37 +0200

    added base project files and base methods

---
+++ source/YoutubeDownloader/YoutubeDownloaderConsole/ConsoleUi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using YoutubeDownloaderCore;
+
+namespace YoutubeDownloaderConsole
+{
+    public class ConsoleUi
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write full path, where do you want to save your file: ");
+            string savePath = Console.ReadLine();
+            Console.WriteLine("Please, paste in the video/playlist link:");
+            string videoLink = Console.ReadLine();
+            Console.WriteLine("Do you want to download audio, video or a playlist (videos,audios)?");
+            Console.WriteLine("Please, write following character, depending on a preferable download option:");
+            Console.WriteLine("a - only audio\nb - video\nc - playlist of videos\nd - playlist of audios" +
+                              "\nany other key to exit program");
+            var option = Console.Read();
+            if (option < 'a' || option > 'd')
+            {
+                Environment.Exit(0);
+            }
+            var downloader = new Downloader(savePath, videoLink);
+            downloader.Download(option);
+        }
+    }
+}
---
+++ source/YoutubeDownloader/YoutubeDownloaderCore/Downloader.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace YoutubeDownloaderCore
+{
+    public class Downloader
+    {
+        private readonly string _saveVideoPath;
+        private readonly string _videoLink;
+        public Downloader(string saveVideoPath, string videoLink)
+        {
+            _saveVideoPath = saveVideoPath;
+            _videoLink = videoLink;
+        }
+
+        public void Download(int option)
+        {
+            switch (option)
+            {
+                case 'a':
+                    break;
+                case 'b':
+                    break;
+                case 'c':
+                    break;
+                case 'd':
+                    break;
+                default:
+                    throw new Exception("No such option implemented!");
+            }
+        }
+
+        private void DownloadAudio()
+        {
+            throw new NotImplementedException("This method is not yet implemented!");
+        }
+
+        private void DownloadVideo()
+        {
+            throw new NotImplementedException("This method is not yet implemented!");
+        }
+
+        private void DownloadAudioPlaylist()
+        {
+            throw new NotImplementedException("This method is not yet implemented!");
+        }
+
+        private void DownloadVideoPlaylist()
+        {
+            throw new NotImplementedException("This method is not yet implemented!");
+        }
+    }
+}
